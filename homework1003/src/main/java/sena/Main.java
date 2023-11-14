3c21e3ee0498a51b16184acab2ac5911b4ce0a24
Serg Chumakov <nexans@ua.fm>
2023-11-14 16:40:42 +0200

    commit20231114_01

---
+++ homework1003/src/main/java/sena/Main.java
@@ -0,0 +1,10 @@
+package sena;
+
+/**
+ * @author Sergej Chumakov on 14.11.2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
