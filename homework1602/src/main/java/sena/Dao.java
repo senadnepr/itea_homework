f7fa8830a3b2b39ed27c1fa43f8dba8f40ddf203
Serg Chumakov <nexans@ua.fm>
2024-01-27 17:18:52 +0200

    commit20240127_02

--- homework1602/src/main/java/sena/Dao.java
+++ homework1602/src/main/java/sena/Dao.java
@@ -15,35 +15,49 @@ public class Dao {
 
     public List<String> sqlRequest(String request) {
 
-        Connection connection = null;
-        Statement statement = null;
+//        Connection connection = null;
+//        Statement statement = null;
         List<String> responseList = new ArrayList<>();
 
-        try {
-            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-            statement = connection.createStatement();
+        try (
+                Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+                Statement statement = connection.createStatement();
+        ) {
 
-            ResultSet resultSet = statement.executeQuery(request);
-            ResultSetMetaData metadata = resultSet.getMetaData();
-            int columnCount = metadata.getColumnCount();
-            while (resultSet.next()) {
-                String row = "";
-                for (int i = 1; i <= columnCount; i++) {
-                    row += resultSet.getString(i) + " ";
+//            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+//            statement = connection.createStatement();
+
+            if (request.contains("SELECT")) {
+                ResultSet resultSet = statement.executeQuery(request);
+
+                ResultSetMetaData metadata = resultSet.getMetaData();
+                int columnCount = metadata.getColumnCount();
+                while (resultSet.next()) {
+                    String row = "";
+                    for (int i = 1; i <= columnCount; i++) {
+                        row += resultSet.getString(i) + " ";
+                    }
+                    responseList.add(row);
                 }
-                responseList.add(row);
+            } else {
+                int res = statement.executeUpdate(request);
+                if (res > 0) {
+                    responseList.add("OK");
+                } else {
+                    responseList.add("ERR");
+                }
             }
-        } catch (
-                SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-                statement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
+//        finally {
+//            try {
+//                connection.close();
+//                statement.close();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
         return responseList;
     }
 }
